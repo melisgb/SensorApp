962b939c6ed37c2a735fda8416625e319fd00f56
melisgb <gloriamelissa1@hotmail.com>
2020-06-11 15:00:50 +0100

    Enabling light sensors
    Pending implementation of light sensors

--- app/src/main/java/com/example/sensorapp/MainActivity.kt
+++ app/src/main/java/com/example/sensorapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.sensorapp
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
