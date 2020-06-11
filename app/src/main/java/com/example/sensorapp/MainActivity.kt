8de02560fff5a24ac660ac1749aa998cbb39bb4d
melisgb <gloriamelissa1@hotmail.com>
2020-06-11 15:48:35 +0100

    Update
    - Setting the sensor type
    - Configuring sensor behaviour at different states of app.
    - Light sensor implemented: When the phone detects a light increase (change) it will play a music

--- app/src/main/java/com/example/sensorapp/MainActivity.kt
+++ app/src/main/java/com/example/sensorapp/MainActivity.kt
@@ -1,23 +1,59 @@
 package com.example.sensorapp
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
+    //Sensor event listener will provide a value everytime sensor changes
+    var sensor : Sensor? = null
+    var sensorManager : SensorManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager!!.unregisterListener(this)
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //TO DO
     }
 
+    var isRunning = false
     override fun onSensorChanged(event: SensorEvent?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if(event!!.values[0]> 40 && !isRunning){
+            isRunning = true
+            try{
+                Toast.makeText(this, "Change detected", Toast.LENGTH_SHORT).show()
+                var mediaPlayer = MediaPlayer()
+                mediaPlayer.setDataSource("https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_2MG.mp3")
+                mediaPlayer.prepare()
+                mediaPlayer.start()
+
+            }catch (ex: Exception){
+                Log.e("MediaPlayer error", ex.message.toString())
+            }
+
+        }
+
     }
 }
